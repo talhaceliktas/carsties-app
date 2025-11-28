3ac5970270a435e5f65fb98d72b1d7a7e7980e6b
talhaceliktas <avrol928@gmail.com>
2025-11-28 15:38:45 +0300

    Add auction model

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -2,5 +2,25 @@
 
 public class Auction
 {
-    
+    public Guid Id { get; set; }
+
+    public int ReservePrice { get; set; } = 0;
+
+    public string? Seller { get; set; }
+
+    public string? Winner { get; set; }
+
+    public int? SoldAmount { get; set; }
+
+    public int CurrentHighBid { get; set; } = 0;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime AuctionEnd { get; set; } = DateTime.UtcNow;
+
+    public Status Status { get; set; }
+
+    public required Item Item { get; set; }
 }
---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,23 @@
+﻿namespace AuctionService.Entities;
+
+public class Item
+{
+    public Guid Id { get; set; }
+
+    public required string Make { get; set; }
+
+    public required string Model { get; set; }
+
+    public int Year { get; set; }
+
+    public required string Color { get; set; }
+
+    public int Mileage { get; set; }
+
+    public required string ImageUrl { get; set; }
+    
+    // Nav Properties
+    public Auction Auction { get; set; }
+    
+    public Guid AuctionId { get; set; }
+}
