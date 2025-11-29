b27d6cfc08aa0741c0ffca73eae855ca665ac338
talhaceliktas <avrol928@gmail.com>
2025-11-29 12:03:09 +0300

    Add DTOs

---
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AuctionService.DTOs;
+
+public class UpdateAuctionDto
+{
+    public required string Make { get; set; }
+
+    public required string Model { get; set; }
+
+    public int Year { get; set; }
+
+    public required string Color { get; set; }
+
+    public int Mileage { get; set; }
+}
