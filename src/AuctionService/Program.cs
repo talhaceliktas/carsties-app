1f74bfa96da4cb8beffd20e7e38c0bee316e00dd
talhaceliktas <avrol928@gmail.com>
2025-11-29 13:44:55 +0300

    Add AuctionsController.cs and routes

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -4,21 +4,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers(options => { options.Conventions.Add(new RoutePrefixConvention("api/v1")); });
 
-builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddAutoMapper(_ => { }, typeof(MappingProfiles));
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
---
+++ src/AuctionService/RequestHelpers/RoutePrefixConvention.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+public class RoutePrefixConvention : IApplicationModelConvention
+{
+    private readonly AttributeRouteModel _routePrefix;
+
+    public RoutePrefixConvention(string prefix)
+    {
+        _routePrefix = new AttributeRouteModel(new RouteAttribute(prefix));
+    }
+
+    public void Apply(ApplicationModel application)
+    {
+        foreach (var controller in application.Controllers)
+        foreach (var selector in controller.Selectors)
+            if (selector.AttributeRouteModel != null)
+                selector.AttributeRouteModel =
+                    AttributeRouteModel.CombineAttributeRouteModel(
+                        _routePrefix, selector.AttributeRouteModel);
+            else
+                selector.AttributeRouteModel = _routePrefix;
+    }
+}
